626964c015bc104a8c8073822bb76a47d992faa6
kashishrajputt <btecg41522029@dseu.ac.in>
2025-07-05 02:02:54 +0530

    landing page

---
+++ app/page.jsx
@@ -0,0 +1,163 @@
+import HeroSection from "@/components/Hero";
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import { faqs } from "@/data/faqs";
+import { features } from "@/data/features";
+import { howItWorks } from "@/data/howItWorks";
+import { ArrowRight } from "lucide-react";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div>
+      <div className="grid-background"></div>
+
+      <HeroSection />
+
+      <section className="w-full py-12 md:py-24 lg:py-32 bg-background"> 
+        
+        <div className="container mx-auto px-4 md:px-6">
+          <h2 className="text-3xl font-bold tracking-tighter text-center mb-12"> Powerful Features for Your Career Growth</h2>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 max-w-6xl mx-auto">
+            {features.map((feature,index)=>{
+            return(
+              <Card key={index}
+              className="border-2 hover:border-primary transition-colors duration-300">
+              
+                <CardContent className="pt-6 text-center flex flex-col items-center ">
+                  <div className="flex flex-col items-center justify-center">{feature.icon}
+                    <h3 className="text-xl font-bold mb-2">{features.title}</h3>
+                    <p className=" foreground">{feature.description}</p>
+
+                  </div>
+                </CardContent>
+                
+              </Card>
+            )
+          })}</div>
+        </div>
+      </section>
+
+      <section className="w-full py-12 md:py-24 bg-muted"> 
+        
+        <div className="container mx-auto px-4 md:px-6">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 max-w-6xl mx-auto">
+            <div className="flex flex-col items-center justify-center space-y-2">
+              <h3 className="text-4xl font-bold">50+</h3>
+              <p className="text-muted-foreground">Industries Covered</p>
+            </div>
+            <div className="flex flex-col items-center justify-center space-y-2">
+              <h3 className="text-4xl font-bold">1000+</h3>
+              <p className="text-muted-foreground">Interview Questions</p>
+            </div>
+            <div className="flex flex-col items-center justify-center space-y-2">
+              <h3 className="text-4xl font-bold">24/7</h3>
+              <p className="text-muted-foreground">AI Support</p>
+            </div>
+            
+          </div>
+        </div>
+      </section>
+
+      <section className="w-full py-8 md:py-12 lg:py-32 bg-background"> 
+        
+        <div className="container mx-auto px-4 md:px-6">
+          <div className="text-center max-w-3xl mx-auto mb-12">
+            <h2 className="text-3xl font-bold mb-4">
+               How It Works
+            </h2>
+            <h1 className="text-muted-foreground font-bold">
+              Four Simple Steps to Accelerate Your Career Growth
+            </h1>
+          </div>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 max-w-6xl mx-auto">
+            {howItWorks.map((item,index)=>{
+            return(
+              <div key={index}
+              className="flex flex-col items-center text-center space-y-4">
+                <div className="w-16 h-16 rounded-full bg-primary/10 flex items-center hustify-center">
+                  {item.icon}
+                </div>
+                <h3 className="font-semibold text-xl">{item.title}</h3>
+                <p className="text-muted-foreground">{item.description}</p>
+              </div>
+            );
+          })}</div>
+        </div>
+      </section>
+
+      <section className="w-full py-8 md:py-12 lg:py-32 bg-background"> 
+        
+        <div className="container mx-auto px-4 md:px-6">
+          <div className="text-center max-w-3xl mx-auto mb-12">
+            <h2 className="text-3xl font-bold mb-4">
+               Frequently Asked Questions
+            </h2>
+            <h1 className="text-muted-foreground font-bold">
+              Find answers to common questions about our platform
+            </h1>
+          </div>
+          <div className=" max-w-6xl mx-auto">
+            <Accordion type="single" collapsible>
+              {faqs.map((faqs,index)=>{
+                return(
+                    <AccordionItem key={index} value={`item-${index}`}>
+                      <AccordionTrigger>{faqs.question}</AccordionTrigger>
+                      <AccordionContent>
+                        {faqs.answer}
+                      </AccordionContent>
+                    </AccordionItem>
+              
+                );
+            })}
+          </Accordion>
+          </div>
+        </div>
+      </section>
+
+      <section className="w-full bg-muted-foreground relative overflow-hidden"> 
+        {/* Top Gradient */}
+          <div
+            aria-hidden
+            className="pointer-events-none absolute left-0 top-0 w-full h-32"
+            style={{
+              background: "linear-gradient(to bottom, oklch(20.791% 0.01814 300.045), transparent 90%)"
+            }}
+          />
+          {/* Bottom Gradient */}
+          <div
+            aria-hidden
+            className="pointer-events-none absolute left-0 bottom-0 w-full h-32"
+            style={{
+              background: "linear-gradient(to top, rgba(0,0,0,0.5), transparent 90%)"
+            }}
+          />
+        <div className="mx-auto py-24 gradient rounded-lg">
+          <div className="flex flex-col items-center justify-center space-y-4 text-center max-w-3xl mx-auto">
+            <h2 className="text-3xl font-bold tracking-tracker text-primary-foreground sm:text-4xl md:text-5xl">
+               Ready to Accelerate Your Career?
+            </h2>
+            <p className="mx-auto max-w-[600px] text-primary-foreground/80 md:text-xl">
+              Join thousands of professionals who are advancing their careers
+              with AI-powered guidance.
+            </p>
+            <Link href="/dashboard" passHref>
+            <Button
+            size="lg"
+            variant="secondary"
+            className="h-11 mt-5 animate-bounce"
+            >Start Your Journey Today<ArrowRight className="ml-2 h-4 w-4" />
+            </Button>
+            </Link>
+          </div>
+          
+        </div>
+      </section>
+
+
+
+    </div>
+      
+  );
+}
---
+++ components/Hero.jsx
@@ -0,0 +1,59 @@
+"use client";
+import Link from "next/link";
+import { Button } from "./ui/button";
+import Image from "next/image";
+import { useEffect, useRef } from "react";
+
+const HeroSection = () => {
+
+
+  return (
+    <section className="w-full pt-36 md:pt-48 pb-10">
+      <div className="space-y-6 text-center">
+        <div className="space-y-6 mx-auto">
+          <h1 className="text-5xl font-bold md:text-6xl lg:text-7xl xl:text-7xl gradient-title">
+            Where Ambition Meets AI-
+            <br />
+            Build the Career You Deserve.
+          </h1>
+          <p className="mx-auto max-w-[600px] text-muted-foreground md:text-xl">
+            Advance your career with personalized guidance, interview prep, and
+            AI-powered tools for job success.
+          </p>
+        </div>
+
+        <div className="flex justify-center"> 
+          <Link href="/dashboard">
+          <Button size='lg' className='px-8' >Get Started</Button>
+          </Link>
+        </div>
+        <div className="hero-image-wrapper relative mt-5 md:mt-0 flex justify-center">
+          <div className="relative w-fit">
+            <Image
+              src={"/banner.jpeg"}
+              width={1280}
+              height={720}
+              alt="Banner"
+              className="rounded-lg shadow-2xl hero-image"
+              priority
+            />
+            <div
+              className="pointer-events-none absolute inset-0 rounded-lg"
+              style={{
+                background: `
+                  radial-gradient(circle at top left, rgb(0,0,0,0.2), transparent 70%),
+                  radial-gradient(circle at top right, rgb(0,0,0,0.2), transparent 70%),
+                  radial-gradient(circle at bottom left, rgb(0,0,0,0.2), transparent 70%),
+                  radial-gradient(circle at bottom right, rgb(0,0,0,0.2), transparent 70%)
+                `
+              }}
+            />
+          </div>
+        </div>
+        </div>
+      
+    </section>
+  );
+}
+
+export default HeroSection;

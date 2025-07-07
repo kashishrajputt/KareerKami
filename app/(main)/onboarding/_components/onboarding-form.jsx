50ef37a3256c3db4e885c2cf046463903897b1b7
kashishrajputt <btech41522029@dseu.ac.in>
2025-07-08 02:02:27 +0530

    onboarding form

--- app/(main)/onboarding/_components/onboarding-form.jsx
+++ app/(main)/onboarding/_components/onboarding-form.jsx
@@ -1,4 +1,25 @@
+"use client";
+
+import { useForm } from "react-hook-form";
+import {zodResolver} from "@hookform/resolvers/zod";
+import { onboardingSchema } from "@/app/lib/schema";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
 const OnboardingPage = ({industries}) => {
+
+
+    const [selectedIndustry, setSelectedIndustry] = useState(null);
+    const router = useRouter();
+    const {
+        register, 
+        handleSubmit, 
+        formState:{errors},
+        setValue,
+        watch,
+        } = useForm({
+        resolver: zodResolver (onboardingSchema),
+    });
 
   return <div>OnboardingForm</div>
   

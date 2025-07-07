d3233674cff57e479285fd2cd2111b9deff56010
kashishrajputt <btech41522029@dseu.ac.in>
2025-07-08 01:34:28 +0530

    onboarding APIs

--- actions/user.js
+++ actions/user.js
@@ -17,15 +17,42 @@ export async function updateUser(data){
         const result = await db.$transaction(
             async() => {
          //find if the industry exists
-         let industryInsights = await tX.industryInsight.findUnique({
+         let industryInsight = await tX.industryInsight.findUnique({
             where:{
                 industry: data.industry,
             },
          })
         // if industry doesnt exit, create it with default values - will replace
         // it with ai later
-        if(!industryInsight)
+        if(!industryInsight){
+            industryInsight = awaittx.industryInsight.create({
+                data: {
+                    industry: data.industry,
+                    salaryRanges: [],
+                    growthRate: 0,
+                    demandLevel: "Medium",
+                    topSkills: [],
+                    marketOutlook: "Neutral",
+                    keyTrends: [],
+                    recommendedSkills: [],
+                    nextUpdate: new Date(Date.now() + 7*24*60*60*100),  // 1 week
+                }, 
+            })
+        }
         // update the user
+        const updatedUser = await tx.user.update({
+            where: {
+                id: user.id,
+            },
+            data:{
+                industry: data.industry,
+                experience: data.experience,
+                bio: data.bio,
+                skills: data.skills,
+            },
+        });
+
+        return { updatedUser, industryInsights };
         },
         {
             timeout: 10000, //default: 5000
@@ -36,5 +63,38 @@ export async function updateUser(data){
     } catch(error){
 console.error("Error updating user and industry:", error.message);
 throw new Error("Failed to update profile")
+    }
+}
+
+export async function getUserOnboardingStatus(){
+    const { userId } = await auth();
+    if(!userId) throw new Error("Unauthorized");
+
+    const user = await db.userfindUnique({
+        where: {
+            clerkUserId: userId,
+        },
+    });
+
+    if (!user) throw new Error("User not found");
+
+    try {
+        const user = await db.user.findUnique({
+            where: {
+                clerkUserId: userId,
+            },
+            select: {
+                industry: true,
+            },
+            
+        });
+        return {
+                isOnboarded: !!user?.industry,
+        };
+        
+
+    } catch(error){
+        console.error("Error checking onboarding status:", error.message);
+        throw new Error("Failed to check onboarding status")
     }
 }

b02a90191004cf5fff4f0a4d1cd563246e7fae88
kashishrajputt <btech41522029@dseu.ac.in>
2025-07-18 22:39:07 +0530

    updates

--- app/(main)/dashboard/layout.js
+++ app/(main)/dashboard/layout.js
@@ -1,25 +1,17 @@
-import React, { Suspense } from 'react'
-import { GridLoader } from 'react-spinners'
+import { BarLoader } from "react-spinners";
+import { Suspense } from "react";
 
-const layout = ({children}) => {
+export default function Layout({ children }) {
   return (
-    <div className='px-5 mt-6'>
-        <div className = "flex items-center justify-between mb-5 ">
-            <h1 className='text-6xl font-bold gradient-title'>Industry Insights</h1>
-        </div>
-        <Suspense
-                fallback={
-                    <div className="mt-8">
-                    <GridLoader width={"100%"} color="gray" />
-                    </div>
-                }
-                >
-                {children}
-            
-        </Suspense>
-        
+    <div className="px-5">
+      <div className="flex items-center justify-between mb-5">
+        <h1 className="text-6xl font-bold gradient-title">Industry Insights</h1>
+      </div>
+      <Suspense
+        fallback={<BarLoader className="mt-4" width={"100%"} color="gray" />}
+      >
+        {children}
+      </Suspense>
     </div>
-  )
+  );
 }
-
-export default layout
